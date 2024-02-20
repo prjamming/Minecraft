609ae868d27ea23c6af29ad8092c184dcd282e82
prjamming <124306301+prjamming@users.noreply.github.com>
2024-02-20 19:10:36 +0000

    Create login-page.js

---
+++ login-page.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "prjamming" && password === "0v3rth3t0p!") {
+        alert("You have successfully logged in.");
+        window.location=”http://MyURLredirectRightPWD.html“;
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
